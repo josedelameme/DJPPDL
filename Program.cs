05afe2eaf6a0024d3e2a4a75187dcc2dc945783d
Jose Moreno <josemanuel.moreno@honeywell.com>
2022-12-03 15:02:42 -0600

    xdxdxd

--- Program.cs
+++ Program.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using YoutubeExplode;
 using DJPPDL.Services;
 using DJPPDL.Menus;
 using DJPPDL.Utils;
 
-using IHost host = Host.CreateDefaultBuilder(args).ConfigureServices(
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, configuration) =>
+    {
+        configuration.Sources.Clear();
+
+        IHostEnvironment env = hostingContext.HostingEnvironment;
+
+        configuration
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+        IConfigurationRoot configurationRoot = configuration.Build();
+
+        TransientFaultHandlingOptions options = new();
+        configurationRoot.GetSection(nameof(TransientFaultHandlingOptions))
+                         .Bind(options);
+
+        Console.WriteLine($"TransientFaultHandlingOptions.Enabled={options.Enabled}");
+        Console.WriteLine($"TransientFaultHandlingOptions.AutoRetryDelay={options.AutoRetryDelay}");
+    })
+    .ConfigureServices(
     (_, services) =>
     services
         .AddSingleton<IStringFormatter, StringFormatter>()
